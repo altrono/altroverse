02b4a00e5d80844c2e5336aea0f1cf11a0393292
Altrono <hbotema16@gmail.com>
2024-04-25 04:06:27 +0200

    Fix bugs

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,16 +10,15 @@ const Navbar = () => {
     variants={navVariants}
     initial="hidden"
     whileInView="show"
-    className={`${styles.xPaddings} py-8 relative`}
-  >
+    className={`${styles.xPaddings} py-8 relative`}>
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div  className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img 
         src="/search.svg" 
         alt="Search logo" 
         className='w-[24px] h-[24px] object-contain'  
         />
-      <h2 className='font-extrabold text-[24px] leading-[30px] text-white'>
+      <h2 className='font-extrabold text-[24px] leading-[30px]text-white'>
         ALTROVERSE
       </h2>
       <img src="/menu.svg" alt="menu" className='w-[24px] h-[24px] object-contain' />
